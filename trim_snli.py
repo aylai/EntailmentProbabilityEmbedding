3058f61c68c45f56103bcc1222119b27de0fec24
aylai2 <aylai2@illinois.edu>
2017-04-25 14:09:46 -0500

    filter snli files

---
+++ trim_snli.py
@@ -0,0 +1,10 @@
+splits = ['train','test','dev']
+
+for s in splits:
+    in_file = open('data/snli/snli_1.0_'+s+'.txt', 'r')
+    out_file = open('data/snli/snli_1.0_'+s+'_trim.txt', 'w')
+    for line in in_file:
+        if not line.startswith("-"):
+            out_file.write(line)
+    in_file.close()
+    out_file.close()
